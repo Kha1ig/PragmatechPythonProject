2e61eb478539ebb53cc4e4a0eaebae2a5cbc7abf
Kha1ig <rehimov.xaliq@internet.ru>
2022-01-17 16:03:05 +0400

    task-02 completed

--- python tasks/task-02/ex-1.py
+++ python tasks/task-02/ex-1.py
@@ -1,10 +1,13 @@
 ### 1-ci tapşırıq:
-reqem = int(input('1-ci reqemi daxil edin: '))
-reqem = int(input('2-ci reqemi daxil edin: '))
-reqem = int(input('3-ci reqemi daxil edin: '))
-reqem = int(input('4-ci reqemi daxil edin: '))
+# Kvadrat yaratmaq olarsa daxil edilən 4 ədəddən, 
+# ekrana kvadratın sahəsini, olmazsa, həmin ədədlərin cəmini verin.
 
-area = pow(reqem, 2)
+reqem1 = int(input('1-ci reqemi daxil edin: '))
+reqem2= int(input('2-ci reqemi daxil edin: '))
+reqem3 = int(input('3-ci reqemi daxil edin: '))
+reqem4 = int(input('4-ci reqemi daxil edin: '))
 
-if area:
-    print(area)
+if reqem1==reqem2 and reqem3 == reqem4 and reqem2 == reqem3 :
+    print(reqem1**2)
+else:
+    print(reqem1+reqem2+reqem3+reqem4)
