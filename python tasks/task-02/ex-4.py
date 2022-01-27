130d24baee15beb8e6ffc05dccf72d014f040778
Kha1ig <rehimov.xaliq@internet.ru>
2022-01-27 22:50:11 +0400

    Some minor amendments

--- python tasks/task-02/ex-4.py
+++ python tasks/task-02/ex-4.py
@@ -18,25 +18,31 @@
 # Əgər yox desə, Murad Əliyev, Uğurlar! Yazılsın.
 
 ad = input('adınızı daxil edin: ')
-if  3 < len(ad) < 11:
-    soyad = input('soyadinizi daxil edin: ')
-    if 5 < len(soyad) < 15:
-        dogum_ili = input('dogum ilinizi daxil edin: ')
-        if len(dogum_ili) == 4:
-            if len(dogum_ili) > 5:
-                    dogum_ili = input('dogum ilini duzgun daxil et: ')
-            e_mail = input('e-mail daxil edin: ')
-            if  10 < len(e_mail) < 22:
-                b = '@gmail.com'
-                mail = e_mail.__add__(b)
-                parol = input('parol daxil edin: ')
-                while 6 > len(parol) <= 13:
-                    if 6 > len(parol) <= 13:
-                        parol = input('parol 6 - 13 simvol arasi daxil edin: ')
-                tesdiq_parolu = input('parolu tesdiqleyin: ')
-                while tesdiq_parolu != parol:
-                    if tesdiq_parolu != parol:
-                        tesdiq_parolu = input('tesdiq parolu duzgun deyil, yendien daxil edin: ')
+while 3 > len(ad) < 11:
+    if  3 > len(ad) < 11:
+        ad = input('adınızı 3 - 11 simvol arasi daxil edin: ')
+soyad = input('soyadinizi daxil edin: ')
+while 5 > len(soyad) < 15:
+    if 5 > len(soyad) < 15:
+        soyad = input('soyadinizi 5 -15 simvol arasi daxil edin: ')
+dogum_ili = int(input('dogum ilinizi daxil edin: '))
+while len(dogum_ili) > 4:
+    if len(dogum_ili) > 4:
+        dogum_ili = int(input('dogum ilini duzgun daxil et: '))
+e_mail = input('e-mail daxil edin: ')
+while 10 > len(e_mail) < 22:
+    if  10 > len(e_mail) < 22:
+        e_mail = input('e-mail 10 - 22 simvol arasi daxil edin: ')
+        b = '@gmail.com'
+        mail = e_mail.__add__(b)
+parol = input('parol daxil edin: ')
+while 6 > len(parol) <= 13:
+    if 6 > len(parol) <= 13:
+        parol = input('parol 6 - 13 simvol arasi daxil edin: ')
+tesdiq_parolu = input('parolu tesdiqleyin: ')
+while tesdiq_parolu != parol:
+    if tesdiq_parolu != parol:
+        tesdiq_parolu = input('tesdiq parolu duzgun deyil, yendien daxil edin: ')
 
 cavab = input('Qeydiyyat tamamlandi ve qeydiyyatın detallarına baxmaq istəyirsinizmi  he ya yox: ')
 print(cavab)
--- python tasks/task-02/t.py
+++ python tasks/task-02/t.py
@@ -1,4 +1,26 @@
+ad = input('adınızı daxil edin: ')
+while 3 > len(ad) < 11:
+    if  3 > len(ad) < 11:
+        ad = input('adınızı 3 - 11 simvol arasi daxil edin: ')
+soyad = input('soyadinizi daxil edin: ')
+while 5 > len(soyad) < 15:
+    if 5 > len(soyad) < 15:
+        soyad = input('soyadinizi 5 -15 simvol arasi daxil edin: ')
+dogum_ili = int(input('dogum ilinizi daxil edin: '))
+while len(dogum_ili) > 4:
+    if len(dogum_ili) > 4:
+        dogum_ili = int(input('dogum ilini duzgun daxil et: '))
 e_mail = input('e-mail daxil edin: ')
-b = '@gmail.com'
-y = e_mail.__add__(b)
-print(y)
+while 10 > len(e_mail) < 22:
+    if  10 > len(e_mail) < 22:
+        e_mail = input('e-mail 10 - 22 simvol arasi daxil edin: ')
+        b = '@gmail.com'
+        mail = e_mail.__add__(b)
+parol = input('parol daxil edin: ')
+while 6 > len(parol) <= 13:
+    if 6 > len(parol) <= 13:
+        parol = input('parol 6 - 13 simvol arasi daxil edin: ')
+tesdiq_parolu = input('parolu tesdiqleyin: ')
+while tesdiq_parolu != parol:
+    if tesdiq_parolu != parol:
+        tesdiq_parolu = input('tesdiq parolu duzgun deyil, yendien daxil edin: ')
