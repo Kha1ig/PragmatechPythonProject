dcf39defcf24ef94e714709d31fe3402ebdde80c
Kha1ig <rehimov.xaliq@internet.ru>
2022-01-29 22:02:43 +0400

    some minor amendments

--- python tasks/task-02/ex-4.py
+++ python tasks/task-02/ex-4.py
@@ -16,48 +16,64 @@
 # (Qeyd: Yuxarıda sizin verdiyiniz şərtlərə uyğun istifadəçi input daxil etmsəsə, hər verdiyibiz şərtə uyğun error tipli mesaj verin. Məsələn doğum ilini 5 simvoldan ibarət daxil etsə, mesaj verilsin ki 4 simvol daxil edin. Yəni hər şərti müvafiq mesajlar ilə userə izah edin. ============================================= Ad: Murad Soyad: Əliyev Yaş: 22 Email: muradaliyev1996@gmail.com Parol: 123456789 
 # ============================================= 
 # Əgər yox desə, Murad Əliyev, Uğurlar! Yazılsın.
+import re 
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'  
 
-ad = input('adınızı daxil edin: ')
-while 3 > len(ad) < 11:
-    if  3 > len(ad) < 11:
-        ad = input('adınızı 3 - 11 simvol arasi daxil edin: ')
-soyad = input('soyadinizi daxil edin: ')
-while 5 > len(soyad) < 15:
-    if 5 > len(soyad) < 15:
-        soyad = input('soyadinizi 5 -15 simvol arasi daxil edin: ')
-dogum_ili = int(input('dogum ilinizi daxil edin: '))
-while len(dogum_ili) > 4:
-    if len(dogum_ili) > 4:
-        dogum_ili = int(input('dogum ilini duzgun daxil et: '))
-e_mail = input('e-mail daxil edin: ')
-while 10 > len(e_mail) < 22:
-    if  10 > len(e_mail) < 22:
-        e_mail = input('e-mail 10 - 22 simvol arasi daxil edin: ')
-        b = '@gmail.com'
-        mail = e_mail.__add__(b)
-parol = input('parol daxil edin: ')
-while 6 > len(parol) <= 13:
-    if 6 > len(parol) <= 13:
-        parol = input('parol 6 - 13 simvol arasi daxil edin: ')
-tesdiq_parolu = input('parolu tesdiqleyin: ')
-while tesdiq_parolu != parol:
-    if tesdiq_parolu != parol:
-        tesdiq_parolu = input('tesdiq parolu duzgun deyil, yendien daxil edin: ')
+def reseption():
+    ad = input('adınızı daxil edin: ')
+    while 3 > len(ad) < 11:
+        if  3 > len(ad) < 11:
+            ad = input('adınızı 3 - 11 simvol arasi daxil edin: ')
+    soyad = input('soyadinizi daxil edin: ')
+    while 5 > len(soyad) < 15:
+        if 5 > len(soyad) < 15:
+            soyad = input('soyadinizi 5 -15 simvol arasi daxil edin: ')
+    dogum_ili = input('dogum ilinizi daxil edin: ')
+    while (len(dogum_ili)) > 4:
+        if len(dogum_ili) > 4:
+            dogum_ili = input('dogum ilini duzgun daxil et: ')
+    e_mail = input('e-mail daxil edin: ')
+    while 10 > len(e_mail) < 22 or e_mail[len(e_mail)-len('@gmail.com')::] != '@gmail.com':
+        if(re.search(regex,e_mail)):   
+            print("Valid Email")   
+        else:   
+            print("Invalid Email")
+        if  10 > len(e_mail) < 22:
+            e_mail = input('e-mail 10 - 22 simvol arasi daxil edin: ')
+    parol = input('parol daxil edin: ')
+    while 6 > len(parol) <= 13:
+        if 6 > len(parol) <= 13:
+            parol = input('parol 6 - 13 simvol arasi daxil edin: ')
+    tesdiq_parolu = input('parolu tesdiqleyin: ')
+    while tesdiq_parolu != parol:
+        if tesdiq_parolu != parol:
+            tesdiq_parolu = input('tesdiq parolu duzgun deyil, yendien daxil edin: ')
 
-cavab = input('Qeydiyyat tamamlandi ve qeydiyyatın detallarına baxmaq istəyirsinizmi  he ya yox: ')
-print(cavab)
+    cavab = input('Qeydiyyat tamamlandi ve qeydiyyatın detallarına baxmaq istəyirsinizmi  he ya yox: ')
+    print(cavab)
+    dic ={
+        'ad':ad,
+        'soyad':soyad,
+        'dogum_ili': dogum_ili,
+        'e-mail': e_mail,
+        'parol': parol
+        
+    }
 
-if cavab == ('he'):
-    print(f'''
-        ad: {ad} 
-        soyad: {soyad}
-        dogum_ili: {dogum_ili} 
-        mail: {mail} 
-        parol: {parol}''' )
-    
-else:
-    print(f'''{ad} {soyad}, _ugurlar!_''')
+    if cavab == ('he'):
+        for k, v in dic.items():
+            print (k, ':', v)
+    else:
+        print(dic.get('ad'),dic.get('soyad'), '_ugurlar!_')
+
+    cavab2 =input('yeni qeydiyyat baslasinmi he ya yox: ')
+    if cavab2 == ('he'):
+        reseption() 
+    else:
+        
+        return dic
+reseption()
+
 
             
             
-    
